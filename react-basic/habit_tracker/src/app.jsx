cc4b2971e3e83ab0163b8ec2670630ccf4e121c3
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-07-19 22:40:37 +0900

    추가 수정

--- react-basic/habit_tracker/src/app.jsx
+++ react-basic/habit_tracker/src/app.jsx
@@ -53,7 +53,10 @@ class App extends Component {
   
   handleReset = () => {
       const habits = this.state.habits.map(habit => {
-        habit.count = 0;
+        if(habit.count !== 0) {
+          return { ...habit, count: 0 };
+        }
+
         return habit;
       });
       this.setState({ habits });
