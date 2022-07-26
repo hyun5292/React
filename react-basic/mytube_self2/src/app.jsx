9812c6ad0ecf8f3772c2839bca8994132db81dd3
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-07-26 22:04:42 +0900

    2차 시도 프로젝트 재생성
    - 디렉토리를 잘못 복사함
    - googleapi 설치 완료

--- react-basic/mytube_self2/src/app.jsx
+++ react-basic/mytube_self2/src/app.jsx
@@ -1,36 +1,12 @@
-import { useEffect, useCallback, useState } from 'react';
 import './app.css';
 import Header from './components/header';
-import Videos from './components/videos';
+import Video from './components/video';
 
-function App({ youtube }) {
-  const [videos, setVideos] = useState([]);
-  const [selectedVideo, setSelectedVideo] = useState([]);
-
-  const search = useCallback(
-    (query) => {
-      setSelectedVideo(null);
-      youtube
-        .search(query)
-        .then((vidoes) => setVideos(videos));
-    },
-    [youtube],
-  );
-
-  useEffect(() => {
-    youtube 
-      .mostPopular()
-      .then((videos) => setVideos(videos));
-  }, [youtube]);
-
-  console.log(videos);
+function App() {
   return (
     <>
       <Header />
-      <Videos
-        videos={videos}
-        display={selectedVideo ? 'list' : 'grid'}
-      />
+      <Video />
     </>
   );
 }
--- react-basic/mytube_self2/src/components/video.jsx
+++ react-basic/mytube_self2/src/components/video.jsx
@@ -2,23 +2,20 @@ import React, { Component } from 'react'
 import styles from '../css/video.module.css';
 
 class Video extends Component {
-  
-  render({ video, video: { snippet }, display }) {
-    const displayType = display === 'list' ? styles.list : styles.grid;
-
+  render() {
     return (
       <div className={styles.video}>
         <img 
             className={styles.video_img} 
-            src={snippet.thumbnails.medium.url} 
+            src={"images/sample.png"} 
             alt="비디오 썸네일 이미지"
         />
         <div className={styles.video_info}>
             <span className={styles.video_info_title}>
-              {snippet.title}
+                Dune Official Trailer
             </span>
             <span className={styles.video_info_singer}>
-              {snippet.channelTitle}
+                Warner Bros. Pictures
             </span>
         </div>
       </div>
