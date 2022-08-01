61a6beda82937a2dbca1f5920b2dc79ab44778e9
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-08-02 01:46:21 +0900

    api 연결
    - postman으로 코드 가져와서 연결

--- react-basic/mytube/src/app.jsx
+++ react-basic/mytube/src/app.jsx
@@ -1,8 +1,23 @@
 import './app.css';
+import React, { useState, useEffect } from 'react';
 import Header from './components/header';
 import Video from './components/video';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyC5ewrm4dgSFclcbwkBPpgxjR2gTZUPbWQ&=AIzaSyC5ewrm4dgSFclcbwkBPpgxjR2gTZUPbWQ", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
   return (
     <>
       <Header />
