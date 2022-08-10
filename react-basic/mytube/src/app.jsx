574d612a7abaf4b43ab4bfb6bf2b2fcbbe889a85
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-08-10 23:47:55 +0900

    리팩토링 완성
    - .env 파일로 key 숨김
    - service/youtube.js로 api 접근 작업 리팩토링 완료
    - 오류 발생
      + createRoot(...): Target container is not a DOM element.

--- react-basic/mytube/src/app.jsx
+++ react-basic/mytube/src/app.jsx
@@ -3,32 +3,19 @@ import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
 import styles from './app.module.css';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
-  useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyC5ewrm4dgSFclcbwkBPpgxjR2gTZUPbWQ&=AIzaSyC5ewrm4dgSFclcbwkBPpgxjR2gTZUPbWQ", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  }, []);
+  useEffect((query) => {    
+    youtube
+      .mostPopular(query)
+      .then(videos => setVideos(videos));
+  });
 
   const search = (query) => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&q=${query}&type=video&key=AIzaSyC5ewrm4dgSFclcbwkBPpgxjR2gTZUPbWQ&=AIzaSyC5ewrm4dgSFclcbwkBPpgxjR2gTZUPbWQ`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube
+      .search(query)
+      .then(videos => setVideos(videos));
   };
 
   return (
