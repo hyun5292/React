b8190cb56f0e9d49141b270426848479b0aa1763
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-08-11 23:33:17 +0900

    로고 클릭 이벤트
    - header 로고 클릭 시 window.location.reload() 실행
      + 이걸 굳이 app.jsx로 올릴 필요는 없겠지...? 아닌가

--- react-basic/mytube/src/app.jsx
+++ react-basic/mytube/src/app.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 import styles from './app.module.css';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
   useEffect(() => {    
     youtube
@@ -18,10 +20,23 @@ function App({ youtube }) {
       .then(videos => setVideos(videos));
   };
 
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
+
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList videos={videos} onVideoClick={selectVideo}/>
+        </div>
+      </section>
     </div>
   );
 }
