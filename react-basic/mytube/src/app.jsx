ca6260ee30936fa8e6f90f8d19578f7a713ed72a
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-08-15 20:53:42 +0900

    memo & useCallback 처리
    useCallback은 메모리에 계속 저장되어있는 것이기 때문에 주의

--- react-basic/mytube/src/app.jsx
+++ react-basic/mytube/src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
 import VideoDetail from "./components/video_detail/video_detail";
@@ -12,16 +12,16 @@ function App({ youtube }) {
     youtube
       .mostPopular()
       .then((videos) => setVideos(videos));
-  }, []);
+  }, [youtube]);
 
-  const search = (query) => {
+  const search = useCallback((query) => {
     youtube
       .search(query)
       .then((videos) => {
         setVideos(videos);
         setSelectedVideo(null);
       });
-  };
+  }, [youtube]);
 
   const selectVideo = (video) => {
     setSelectedVideo(video);
