2a7d8c6e3309724036ffcb118e95a35f5160f902
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-08-16 20:31:16 +0900

    fetch와 axios 비교

--- react-basic/mytube/src/index.js
+++ react-basic/mytube/src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
+import axios from 'axios';
 import Youtube from './service/youtube';
 
-const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+const httpClient = axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+});
+const youtube = new Youtube(httpClient);
+// const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
