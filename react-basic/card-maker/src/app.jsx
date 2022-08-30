892130e9bea5d36c18d8d06c867dce29060de9d0
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-08-30 23:35:39 +0900

    react-router-dom 다운그레이드
    npm install react-router-dom@5.3.0

--- react-basic/card-maker/src/app.jsx
+++ react-basic/card-maker/src/app.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import styles from './app.module.css';
 import Login from './component/login/login';
 
 function App({ authService }) {
   return (
     <div className={styles.app}>
-      <Login authService={authService}/>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService}/>
+          </Route>
+          <Route path="/maker">
+
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ react-basic/card-maker/src/maker/maker.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Maker = (props) => {
+    return (
+        <h1>Maker</h1> 
+    );           
+};
+
+export default Maker;
