6f4dd243da45c4c5260d6d1625e89d867eee9e39
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-09-14 23:38:55 +0900

    card & card_edit_form 작업
    파일 생성 완료

---
+++ react-basic/card-maker/src/components/card/card.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './card.module.css';
+
+const Card = ({ card }) => <section><h1>{card.id}</h1></section>;
+
+export default Card;
--- react-basic/card-maker/src/components/maker/maker.jsx
+++ react-basic/card-maker/src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Editor from '../editor/editor';
@@ -8,6 +8,41 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+    const [cards, setCards] = useState([
+        {
+            id: '1',
+            name: 'Grog',
+            company: 'LG',
+            theme: 'light',
+            title: 'Front Developer',
+            email: 'tytyjacob@naver.com',
+            message: 'I am Grog',
+            fileName: 'Grog',
+            fileURL: 'url'
+        },
+        {
+            id: '2',
+            name: 'Grog',
+            company: 'LG',
+            theme: 'light',
+            title: 'Front Developer',
+            email: 'tytyjacob@naver.com',
+            message: 'I am Grog',
+            fileName: 'Grog',
+            fileURL: 'url'
+        },
+        {
+            id: '3',
+            name: 'Grog',
+            company: 'LG',
+            theme: 'light',
+            title: 'Front Developer',
+            email: 'tytyjacob@naver.com',
+            message: 'I am Grog',
+            fileName: 'Grog',
+            fileURL: 'url'
+        },
+    ]);
     const navigate = useNavigate();
     const onLogout = () => {
         authService.logout();
@@ -25,8 +60,8 @@ const Maker = ({ authService }) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
                 <div className={styles.container}>
-                    <Editor />
-                    <Preview />
+                    <Editor cards={cards} />
+                    <Preview cards={cards} />
                 </div>
             <Footer />
         </section>
