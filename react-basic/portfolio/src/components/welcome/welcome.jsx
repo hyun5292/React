75a0f7c68e8bf56ece3d8cf9f50aaa38a1ce20c8
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-10-14 00:15:47 +0900

    welcome 1차 완료

--- react-basic/portfolio/src/components/welcome/welcome.jsx
+++ react-basic/portfolio/src/components/welcome/welcome.jsx
@@ -18,63 +18,74 @@ const Welcome = (props) => (
         <Title txtTitle="contact me" chkMb={false} />
         <p className={styles.sub}>깃허브 링크를 클릭해주세요!</p>
       </div>
-      <div className={styles.cont}>
-        <img className={styles.selfie} src="/images/selfie.png" alt="사진" />
-        <div className={styles.line}></div>
-        <div className={styles.explain}>
-          <div className={styles.intro}>
-            <p className={styles.intro_title}>
-              안녕하세요
-              <br />
-              즐거움을 찾는 개발자 박수현입니다.
-            </p>
-            <p className={styles.intro_sect}>
-              &nbsp;저의 좌우명은 '즐거움'입니다
-              <br />
-              어떤 일이든 즐기면서 할 줄 알고
-              <br />
-              끊임없이 공부하는 개발자가 되겠습니다!
-            </p>
-          </div>
-          <div className={styles.info_wrap}>
-            <ul className={styles.info_list}>
-              <li className={styles.info_item}>
-                <div className={styles.info}>
-                  <FontAwesomeIcon icon={faCalendar} />
-                  <div className={styles.info_text}>생년월일</div>
-                </div>
-                <div className={styles.data}>1998.05.12</div>
-              </li>
-              <li className={styles.info_item}>
-                <div className={styles.info}>
-                  <FontAwesomeIcon icon={faMobileAlt} />
-                  <div className={styles.info_text}>전화번호</div>
-                </div>
-                <div className={styles.data}>010-9944-5292</div>
-              </li>
-              <li className={styles.info_item}>
-                <div className={styles.info}>
-                  <FontAwesomeIcon icon={faAt} />
-                  <div className={styles.info_text}>이메일</div>
-                </div>
-                <div className={styles.data}>tytyjacob@naver.com</div>
-              </li>
-              <li className={styles.info_item}>
-                <div className={styles.info}>
-                  <FontAwesomeIcon icon={faGithub} />
-                  <div className={styles.info_text}>Git</div>
-                </div>
-                <div className={styles.data}>
-                  <a
-                    className={styles.gitLink}
-                    href="https://github.com/hyun5292"
-                  >
-                    <FontAwesomeIcon icon={faLink} />
-                    https://github.com/hyun5292
-                  </a>
-                </div>
-              </li>
-            </ul>
+      <div className={styles.card_border}>
+        <div className={styles.cont}>
+          <img className={styles.selfie} src="/images/selfie.png" alt="사진" />
+          <div className={styles.line}></div>
+          <div className={styles.explain}>
+            <div className={styles.intro}>
+              <p className={styles.intro_title}>
+                안녕하세요
+                <br />
+                <span className={styles.funny}>즐거움</span>을 찾는 개발자{" "}
+                <span className={styles.myName}>박수현</span>입니다.
+              </p>
+              <p className={styles.intro_sect}>
+                &nbsp;저의 좌우명은 '즐거움'입니다
+                <br />
+                어떤 일이든 즐기면서 할 줄 알고
+                <br />
+                끊임없이 공부하는 개발자가 되겠습니다!
+              </p>
+            </div>
+            <div className={styles.info_wrap}>
+              <ul className={styles.info_list}>
+                <li className={styles.info_item}>
+                  <div className={styles.info}>
+                    <div className={styles.info_icon}>
+                      <FontAwesomeIcon icon={faCalendar} />
+                    </div>
+                    <div className={styles.info_text}>생년월일</div>
+                  </div>
+                  <div className={styles.data}>1998.05.12</div>
+                </li>
+                <li className={styles.info_item}>
+                  <div className={styles.info}>
+                    <div className={styles.info_icon}>
+                      <FontAwesomeIcon icon={faMobileAlt} />
+                    </div>
+                    <div className={styles.info_text}>전화번호</div>
+                  </div>
+                  <div className={styles.data}>010-9944-5292</div>
+                </li>
+                <li className={styles.info_item}>
+                  <div className={styles.info}>
+                    <div className={styles.info_icon}>
+                      <FontAwesomeIcon icon={faAt} />
+                    </div>
+                    <div className={styles.info_text}>이메일</div>
+                  </div>
+                  <div className={styles.data}>tytyjacob@naver.com</div>
+                </li>
+                <li className={styles.info_item}>
+                  <div className={styles.info}>
+                    <div className={styles.info_icon}>
+                      <FontAwesomeIcon icon={faGithub} />
+                    </div>
+                    <div className={styles.info_text}>Git</div>
+                  </div>
+                  <div className={styles.data}>
+                    <a
+                      className={styles.gitLink}
+                      href="https://github.com/hyun5292"
+                    >
+                      <FontAwesomeIcon icon={faLink} />
+                      https://github.com/hyun5292
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
