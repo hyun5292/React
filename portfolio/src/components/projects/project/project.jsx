32957f253300117ee03ac3db73fd33dc068b579f
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-10-26 23:52:03 +0900

    projects 디자인 시작
    - 화살표 색상 안바뀜..왜?

---
+++ portfolio/src/components/projects/project/project.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./project.module.css";
+
+const Project = (props) => <div className={styles.project}>프로젝트</div>;
+
+export default Project;
--- portfolio/src/components/projects/projects.jsx
+++ portfolio/src/components/projects/projects.jsx
@@ -2,12 +2,35 @@ import React from "react";
 import Title from "../title/title";
 import styles from "./projects.module.css";
 import sectStyle from ".././sect.module.css";
+import Project from "./project/project";
+import { GrPrevious, GrNext } from "react-icons/gr";
 
-const Projects = (props) => (
-  <div className={`${styles.projects} ${sectStyle.projects}`}>
-    <Title txtTitle="Projects" txtColor="grey" />
-    <h1>글씨입니다.</h1>
-  </div>
-);
+const Projects = (props) => {
+  const files = [
+    {
+      fileName: "프로젝트1",
+      fileImg: "/images/selfie.png",
+      fileInfo:
+        "프로젝트 설명이 길어야하니까 어쩔 수 없이 이렇게 길게 설명을 한 번 써보기는 하는데 얼마나 길게 써질지는 모르겠네",
+    },
+    {
+      fileName: "프로젝트2",
+      fileImg: "/images/selfie.png",
+      fileInfo:
+        "프로젝트 설명이 길어야하니까 어쩔 수 없이 이렇게 길게 설명을 한 번 써보기는 하는데 얼마나 길게 써질지는 모르겠네",
+    },
+  ];
+
+  return (
+    <div className={`${styles.projects} ${sectStyle.projects}`}>
+      <Title txtTitle="Projects" txtColor="grey" />
+      <div className={styles.cont}>
+        <GrPrevious className={styles.arrow} />
+        <Project />
+        <GrNext className={styles.arrow} />
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
