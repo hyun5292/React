1b6d65e45b806d8c97bd954e6f0a1cea5c91ade7
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-11-01 02:46:54 +0900

    페이지 이동 이벤트 시도
    - style이 없다고 안된다 오류 뜸
    - 일단 밑에 circle부터 처리해야할 듯

--- portfolio/src/components/projects/projects.jsx
+++ portfolio/src/components/projects/projects.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Title from "../title/title";
 import styles from "./projects.module.css";
 import sectStyle from ".././sect.module.css";
 import Project from "./project/project";
 import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
 import { FaCircle, FaRegCircle } from "react-icons/fa";
+import { useEffect } from "react";
 
 const Projects = (props) => {
-  const [pages, setPages] = useState([true, false, false]);
-
+  const pageRef = useRef();
+  const [pageCnt, setPageCnt] = useState(1);
   const files = [
     {
       fileNum: "1",
@@ -42,6 +43,29 @@ const Projects = (props) => {
     },
   ];
 
+  const handleNextPg = () => {
+    const nowPg = pageCnt;
+    if (nowPg < files.length) {
+      setPageCnt(nowPg + 1);
+      // pageRef.current.style.transform = `translateX(${
+      //   window.innerWidth * (nowPg - 1)
+      // })`;
+    }
+  };
+  const handlePrevPg = () => {
+    const nowPg = pageCnt;
+    if (nowPg > 1) {
+      setPageCnt(nowPg - 1);
+      // pageRef.current.style.transform = `translateX(${
+      //   window.innerWidth * (nowPg - 1)
+      // })`;
+    }
+  };
+
+  useEffect(() => {
+    console.log("pageCnt = ", pageCnt);
+  }, [pageCnt, files]);
+
   return (
     <div className={`${styles.projects} ${sectStyle.projects}`}>
       <Title
@@ -49,26 +73,34 @@ const Projects = (props) => {
         txtSub="양 옆 버튼을 눌러주세요!"
         txtColor="projects"
       />
-      <div className={styles.cont}>
-        <MdNavigateBefore className={`${styles.arrow} ${styles.prev}`} />
-        {files.map((file) => {
-          return (
-            <Project
-              className={styles.project}
-              key={file.fileNum}
-              file={file}
-            />
-          );
-        })}
-        <MdNavigateNext className={`${styles.arrow} ${styles.next}`} />
+      <div className={styles.cont} pageRef={pageRef}>
+        <MdNavigateBefore
+          onClick={handlePrevPg}
+          className={`${styles.arrow} ${styles.prev}`}
+        />
+        {files &&
+          files.map((file) => {
+            return (
+              <Project
+                className={styles.project}
+                key={file.fileNum}
+                file={file}
+              />
+            );
+          })}
+        <MdNavigateNext
+          onClick={handleNextPg}
+          className={`${styles.arrow} ${styles.next}`}
+        />
       </div>
-      {pages.map((pg) =>
-        pg ? (
-          <FaCircle className={styles.circles} />
-        ) : (
-          <FaRegCircle className={styles.circles} />
-        )
-      )}
+      {files &&
+        files.map((file) =>
+          pageCnt === file.fileNum ? (
+            <FaCircle className={styles.circles} />
+          ) : (
+            <FaRegCircle className={styles.circles} />
+          )
+        )}
     </div>
   );
 };
