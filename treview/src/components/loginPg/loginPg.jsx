29b214c92115cebcb3b7dcb5be8457adaed0158b
박수현 <57443458+hyun5292@users.noreply.github.com>
2023-01-11 23:40:29 +0900

    treview loginPg 디자인 중

--- treview/src/components/loginPg/loginPg.jsx
+++ treview/src/components/loginPg/loginPg.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import styles from "./loginPg.module.css";
+import { FaArrowLeft } from "react-icons/fa";
 
 const LoginPg = (props) => {
-  return <div className={styles.loginPg}>LoginPg</div>;
+  return (
+    <div className={styles.loginPg}>
+      <div className={styles.loginCont}>
+        <div className={styles.formCont}>
+          <button className={styles.backBtn}>
+            <FaArrowLeft className={styles.icon} />
+          </button>
+          <form action="#">
+            <div className={styles.inputNm}>아이디</div>
+            <input type="text" placeholder="아이디" />
+            <div className={styles.inputNm}>비밀번호</div>
+            <input type="password" placeholder="비밀번호" />
+            <button className={styles.doLoginBtn}>로그인</button>
+          </form>
+        </div>
+        <span className={styles.logo}>Truck + Review</span>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPg;
