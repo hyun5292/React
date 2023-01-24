e9d9070c604580d684b3c53d6c33bf4f9cb23879
박수현 <57443458+hyun5292@users.noreply.github.com>
2023-01-25 02:15:23 +0900

    treview mainPg
    - 배경은 사진으로 지금처럼 하고 parallax로 효과 주자

--- treview/src/components/mainPg/mainPg.jsx
+++ treview/src/components/mainPg/mainPg.jsx
@@ -10,16 +10,41 @@ const MainPg = ({}) => {
       <div className={styles.search}>
         <Search />
       </div>
-      <div className={styles.introCont}>
-        <div className={styles.title}>트리뷰(Treview)란?</div>
-        트럭(Truck) + 리뷰(Review)의 줄임말로
-        <br />
-        경기도 내 일반 화물업체(이사화물이 아닌 화물)의 운송을 주선하는
-        업체(소)의
-        <br />
-        정보를 검색할 수 있고
-        <br />
-        공장별 리뷰를 남길 수 있는 사이트입니다.
+      <div className={`${styles.introCont} ${styles.intro1}`}>
+        <p>
+          트리뷰(Treview)란? 트럭(Truck) + 리뷰(Review)의 줄임말로
+          <br />
+          경기도 내 일반 화물업체(이사화물이 아닌 화물)의 운송을 주선하는
+          업체(소)의
+          <br />
+          정보를 검색할 수 있고
+          <br />
+          공장별 리뷰를 남길 수 있는 사이트입니다.
+        </p>
+      </div>
+      <div className={`${styles.introCont} ${styles.intro2}`}>
+        <p>
+          트리뷰(Treview)란? 트럭(Truck) + 리뷰(Review)의 줄임말로
+          <br />
+          경기도 내 일반 화물업체(이사화물이 아닌 화물)의 운송을 주선하는
+          업체(소)의
+          <br />
+          정보를 검색할 수 있고
+          <br />
+          공장별 리뷰를 남길 수 있는 사이트입니다.
+        </p>
+      </div>
+      <div className={`${styles.introCont} ${styles.intro3}`}>
+        <p>
+          트리뷰(Treview)란? 트럭(Truck) + 리뷰(Review)의 줄임말로
+          <br />
+          경기도 내 일반 화물업체(이사화물이 아닌 화물)의 운송을 주선하는
+          업체(소)의
+          <br />
+          정보를 검색할 수 있고
+          <br />
+          공장별 리뷰를 남길 수 있는 사이트입니다.
+        </p>
       </div>
     </div>
   );
