810de394f2dfdf885f2c8096c48c421c58ce50f0
박수현 <57443458+hyun5292@users.noreply.github.com>
2023-01-26 23:35:30 +0900

    treview mainPg
    - 세부적인 디자인 중
    - 사이트 설명, 제작동기, 또 뭐하지

--- treview/src/components/mainPg/mainPg.jsx
+++ treview/src/components/mainPg/mainPg.jsx
@@ -6,45 +6,35 @@ import pgStyle from "../../css/page.module.css";
 const MainPg = ({}) => {
   return (
     <div className={`${styles.mainPg} ${pgStyle.pg} ${pgStyle.pgPadding}`}>
-      <span className={styles.backImg}></span>
       <div className={styles.search}>
         <Search />
       </div>
-      <div className={`${styles.introCont} ${styles.intro1}`}>
-        <p>
-          트리뷰(Treview)란? 트럭(Truck) + 리뷰(Review)의 줄임말로
-          <br />
-          경기도 내 일반 화물업체(이사화물이 아닌 화물)의 운송을 주선하는
-          업체(소)의
-          <br />
-          정보를 검색할 수 있고
-          <br />
-          공장별 리뷰를 남길 수 있는 사이트입니다.
-        </p>
-      </div>
-      <div className={`${styles.introCont} ${styles.intro2}`}>
-        <p>
-          트리뷰(Treview)란? 트럭(Truck) + 리뷰(Review)의 줄임말로
-          <br />
-          경기도 내 일반 화물업체(이사화물이 아닌 화물)의 운송을 주선하는
-          업체(소)의
-          <br />
-          정보를 검색할 수 있고
-          <br />
-          공장별 리뷰를 남길 수 있는 사이트입니다.
-        </p>
-      </div>
-      <div className={`${styles.introCont} ${styles.intro3}`}>
-        <p>
-          트리뷰(Treview)란? 트럭(Truck) + 리뷰(Review)의 줄임말로
-          <br />
-          경기도 내 일반 화물업체(이사화물이 아닌 화물)의 운송을 주선하는
-          업체(소)의
-          <br />
-          정보를 검색할 수 있고
-          <br />
-          공장별 리뷰를 남길 수 있는 사이트입니다.
-        </p>
+      <div className={styles.introCont}>
+        <div className={`${styles.introItem} ${styles.intro1}`}>
+          <p className={styles.introTitle}>트리뷰(Treview)란?</p>
+          <p>
+            트럭(Truck) + 리뷰(Review)의 줄임말로
+            <br />
+            경기도 내 일반 화물업체(이사화물이 아닌 화물)의 운송을 주선하는
+            <br />
+            업체(소)의 정보를 검색할 수 있고
+            <br />
+            공장별 리뷰를 남길 수 있는 사이트입니다
+          </p>
+        </div>
+        <div className={`${styles.introItem} ${styles.intro2}`}>
+          <p className={styles.introTitle}>제작 동기는?</p>
+          <p>
+            화물차 기사이신 아빠가
+            <br />
+            매일 저녁마다 항상 공장마다의 특징, 직원들, 현장 상태 등을
+            말씀하시고
+            <br />
+            동료 기사분들하고 공유한다는 얘기를 듣고
+            <br />
+            사이트를 만들면 좋겠다고 생각해서 만들게 되었습니다
+          </p>
+        </div>
       </div>
     </div>
   );
