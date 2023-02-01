58caf4809ee9a3c05d5e80767b048e1b7586128b
박수현 <57443458+hyun5292@users.noreply.github.com>
2023-02-01 23:27:30 +0900

    treview mainPg API 중단 문구 추가

--- treview/src/components/mainPg/mainPg.jsx
+++ treview/src/components/mainPg/mainPg.jsx
@@ -3,7 +3,7 @@ import styles from "./mainPg.module.css";
 import Search from "../facSearchBar/facSearchBar.jsx";
 import pgStyle from "../../css/page.module.css";
 
-const MainPg = ({}) => {
+const MainPg = (props) => {
   return (
     <div className={`${styles.mainPg} ${pgStyle.pg} ${pgStyle.pgPadding}`}>
       <div className={styles.search}>
@@ -18,6 +18,11 @@ const MainPg = ({}) => {
             경기도 내 일반 화물업체(이사화물이 아닌 화물)의 운송을 주선하는
             업체(소)의 정보를 검색할 수 있고 공장별 리뷰를 남길 수 있는
             사이트입니다
+          </p>
+          <p className={styles.apiInfo}>
+            해당 API는 지방행정데이터개방시스템(http://www.localdata.kr) 전면
+            개편(데이터 통합 및 데이터 개방 중단)에 따라 데이터 갱신이 2018년
+            9월 6일자로 중지되었습니다
           </p>
         </div>
         <div className={`${styles.introItem} ${styles.intro2}`}>
