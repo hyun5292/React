a1fac77ce02f04c4e0ffa2d29b0c844795959ecf
박수현 <57443458+hyun5292@users.noreply.github.com>
2023-04-20 02:35:55 +0900

    treview data 검색 성공 & reset 버튼 & react-table
    - mainPg에서 검색 후 searchPg 데이터 이동(?) 완료
    - reset 버튼 추가
    - react-table 설치
    - searchPg에 getSearchedList 추가 완료
    - searchPg에 getAllList 추가 완료
    
    + 디자인 수정 필요

--- treview/src/components/factory/factory.jsx
+++ treview/src/components/factory/factory.jsx
@@ -4,22 +4,38 @@ import { Outlet, useNavigate } from "react-router-dom";
 const Factory = ({ factoryDB }) => {
   const navigate = useNavigate();
 
+  const getSearchedList = (query) => {
+    return factoryDB.getSearchedList(query).then((list) => {
+      return list;
+    });
+  };
+
+  const getAllList = () => {
+    return factoryDB.getAllList().then((dataList) => {
+      return dataList;
+    });
+  };
+
   const goSearch = (query) => {
     navigate(`/search`, { state: { query } });
   };
 
-  const getSearchedList = (query) => {
-    const data = factoryDB.getSearchedList(query).then((list) => {
-      return list;
-    });
-    console.log(data);
-    return data;
-  };
+  // const getSearchedList = (query) => {
+  //   const data = factoryDB.getSearchedList(query).then((list) => {
+  //     return list;
+  //   });
+  //   console.log(data);
+  //   return data;
+  // };
 
   return (
     <div>
       <Outlet
-        context={{ goSearch: goSearch, getSearchedList: getSearchedList }}
+        context={{
+          goSearch: goSearch,
+          getSearchedList: getSearchedList,
+          getAllList: getAllList,
+        }}
       />
     </div>
   );
--- treview/src/components/factory_search/factory_search.jsx
+++ treview/src/components/factory_search/factory_search.jsx
@@ -1,18 +1,35 @@
 import React, { useEffect, useState } from "react";
+import FSearchBar from "../factory_search_bar/factory_search_bar";
+import FactoryTable from "../factory_table/factory_table";
 import { useLocation, useOutletContext } from "react-router-dom";
 import styles from "./factory_search.module.css";
+import { BsArrowClockwise } from "react-icons/bs";
 
 const FactorySearch = (props) => {
   const location = useLocation("");
-  const { getSearchedList } = useOutletContext();
+  const { getSearchedList, getAllList } = useOutletContext();
   const [data, setData] = useState([]);
-  const { sigun, fName } = location.state.query;
 
   useEffect(() => {
-    setData(getSearchedList({ sigun: sigun, fName: fName }));
-  }, []);
+    location.state
+      ? getSearchedList({
+          sigun: location.state.query.sigun,
+          fName: location.state.query.fName,
+        }).then((result) => setData(result))
+      : getAllList().then((result) => setData(result));
+  }, [getAllList, getSearchedList, location.state]);
 
-  return <div className={styles.factorySearch}></div>;
+  return (
+    <div className={styles.factorySearch}>
+      <div className={styles.searchBar}>
+        <FSearchBar />
+        <button className={styles.resetCont}>
+          <BsArrowClockwise className={styles.resetIcon} />
+        </button>
+      </div>
+      <FactoryTable data={data} />
+    </div>
+  );
 };
 
 export default FactorySearch;
