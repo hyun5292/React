81f2d8004c726dc01bde1777b50c19f0b2cad66c
박수현 <57443458+hyun5292@users.noreply.github.com>
2023-05-18 23:54:21 +0900

    treivew 반응형
    - header, menu hamburger 처리 완료

--- treview/src/components/header/header.jsx
+++ treview/src/components/header/header.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Menu from "../header_menu/header_menu";
 import styles from "./header.module.css";
 
 const Header = ({ uData, onLogout }) => {
+  const [onMenu, setOnMenu] = useState(false);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    const newResult = !onMenu;
+    setOnMenu(newResult);
+  };
+
   return (
     <div className={styles.header}>
       <img
@@ -10,7 +18,21 @@ const Header = ({ uData, onLogout }) => {
         src="../images/truck_long.png"
         alt="사이트 로고"
       />
-      <Menu uData={uData} onLogout={onLogout} />
+      <div
+        className={onMenu ? styles.hamburger : styles.hamburger_off}
+        onClick={handleClick}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div
+        className={`${styles.menu} ${
+          onMenu ? styles.menu_on : styles.menu_off
+        }`}
+      >
+        <Menu uData={uData} onLogout={onLogout} />
+      </div>
     </div>
   );
 };
