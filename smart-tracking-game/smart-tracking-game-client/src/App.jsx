7ff8c7f27228bfb43421e8952edd067998cce950
박수현 <57443458+hyun5292@users.noreply.github.com>
2023-06-05 22:34:15 +0900

    SmartTracking 시작
    - 프로젝트 생성 및 기획 중

---
+++ smart-tracking-game/smart-tracking-game-client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.module.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>스마트한 추적 놀이 - 클라이언트</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ smart-tracking-game/smart-tracking-game-manager/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.module.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>스마트한 추적 놀이 - 관리자</h1>
+    </div>
+  );
+}
+
+export default App;
